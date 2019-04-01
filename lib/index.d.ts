8311cbd437ad637a39eaaa4fc7e7aef7e6fd27f9
badaei@yahoo.com <badaei@yahoo.com>
2019-03-31 22:19:10 -0700

    One match, advanced type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,8 @@
-export declare function run<T, Obj, Key extends keyof Obj>(obj: Obj & {
-    [key: string]: () => T;
-}, key: Key): T;
-export declare type Behavior<Pos extends string, Out> = Record<Pos, () => Out>;
+declare type NonDistributiveMatchLeft<A, B, C> = [A] extends [B] ? C : never;
+declare type NonDistributiveMatchBoth<A, B, C> = NonDistributiveMatchLeft<A, B, C> & NonDistributiveMatchLeft<B, A, C>;
+declare type RecordOfFunctions<Out> = {
+    [_ in string | number]: () => Out;
+};
+export declare function match<Out, O extends RecordOfFunctions<Out>, K>(obj: O extends RecordOfFunctions<Out> ? O : O, key: K & NonDistributiveMatchBoth<K, keyof O, K> & keyof O): Out;
+export declare function match<Out, O extends RecordOfFunctions<Out>>(obj: O extends RecordOfFunctions<Out> ? O : O): <K>(key: K & NonDistributiveMatchBoth<K, keyof O, K> & keyof O) => Out;
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
-// Exhaustive Literal Type Checking
-export function run< T, Obj, Key extends keyof Obj >( 
-  obj: Obj & {[key: string]: () => T }, 
-  key: Key 
-): T {
-  return obj[ key ]()
+type NonDistributiveMatchLeft<A,B,C> = [A] extends [B] ? C : never
+type NonDistributiveMatchBoth<A,B,C> = NonDistributiveMatchLeft<A,B,C> & NonDistributiveMatchLeft<B,A,C>
+type RecordOfFunctions<Out> = {[ _ in string |number ]: () => Out}
+export function match<Out, O extends RecordOfFunctions<Out>, K >( obj: O extends RecordOfFunctions<Out> ? O : O, key: K & NonDistributiveMatchBoth<K, keyof O, K> & keyof O ): Out 
+export function match<Out, O extends RecordOfFunctions<Out> >( obj: O extends RecordOfFunctions<Out> ? O : O ) : <K>( key: K & NonDistributiveMatchBoth<K, keyof O, K> & keyof O ) => Out 
+export function match<Out, O extends RecordOfFunctions<Out>, K >( obj: O extends RecordOfFunctions<Out> ? O : O, key?: K & NonDistributiveMatchBoth<K, keyof O, K> & keyof O ): ( (key: keyof O) => Out ) | Out {
+  if( key === undefined) {
+    return ( key: keyof O ) => obj[ key ]()
+  } else {
+    return obj[ key ]()
+  }
 }
-
-export type Behavior< Pos extends string , Out > = Record< Pos, () => Out >
-
-function literalMatch< Out, Key extends string | number = ( string | number ) >(
-  key: Key,
-  obj: {[k in Key]: () => Out } 
-): Out {
-  return obj[ key ]()
-}
-
